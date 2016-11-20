3adcbce86167cf94c6987bf9ca81a4127b7ef8fc
Matthew Preucil <mattpsbs@gmail.com>
2016-11-19 18:33:54 -0800

    Remove www from Package.swift

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
     name: "Sockets",
     dependencies: [
-		.Package(url: "https://www.github.com/krzyzanowskim/CryptoSwift", majorVersion: 0),
-		.Package(url: "https://www.github.com/randymarsh77/cast", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/idisposable", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/scope", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/time", majorVersion: 1),
+		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift", majorVersion: 0),
+		.Package(url: "https://github.com/randymarsh77/cast", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/idisposable", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/scope", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/time", majorVersion: 1),
 	]
 )
